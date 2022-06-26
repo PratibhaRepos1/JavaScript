407dc54302c5cd61ce57fdad47745eaf2d79083f
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2022-06-26 11:10:07 +0700

    JS datatypes

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,13 @@
+/**
+ Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+ */
+
+let country = 'Germany';
+let continent = 'Europe';
+let population = '83.24 million';
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+// 7 Primitive data types in javascript
+let js = 'amazing';
+if (js === 'amazing') alert('Java script is fun and more!');
+console.log(40 + 8 + 23 - 10);
+//10. Values and Variables
+let firstName = 'Bob';
+console.log(firstName);
+
+//constant
+let PI = 3.1415;
+
+//variables more example
+let myFirstJob = "Programmer";
+let myCurrentJob = "IT service";
+
+//1. Number 2. String 
+//3. Boolean
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+
