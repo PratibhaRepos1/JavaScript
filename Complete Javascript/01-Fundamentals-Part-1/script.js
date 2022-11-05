97518edeaff854585b301bc4ede6a15a986f567c
Pratibha <pratibha.jadhav3229@gmail.com>
2022-11-05 12:20:11 +0700

    values and Variables

---
+++ Complete Javascript/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,31 @@
+let js = 'amazing'
+//Values and Varibles
+//Values - smallest unit of of information
+console.log('Pratibha');
+console.log(26);
+let firstName = 'Pratibha';
+let lastName = 'Jadhav';
+
+//Varibles hold the values like varibles are box can contain or hold a values.
+let PI = 3.1415;
+
+console.log('My Name is ' + firstName + ' ' + lastName);
+let myFirstJob = 'Programmer';
+let mycurrentJob = 'Senior Programmer';
+
+console.log(myFirstJob);
+console.log(mycurrentJob);
+
+/*
+. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console */
+
+let country = 'India';
+let continent = 'Asia';
+let population = 1417173173;
+let populationInBillion = '1.393 billion';
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(populationInBillion);
