6b80fe55d3786fb5bfbfd7f0606f9ecaba6cec9b
Pratibha <pratibha.jadhav3229@gmail.com>
2022-11-06 11:25:53 +0700

    coding challenge #1

---
+++ Complete Javascript/01-Fundamentals-Part-1/operators.js
@@ -0,0 +1,29 @@
+// operators in javascript
+//Math operators
+const now = 2037;
+const agePratibha = now - 1984;
+const ageDaksh = now - 2012;
+
+console.log(agePratibha, ageDaksh);
+console.log(agePratibha * 2, ageDaksh / 5);
+console.log(2 ** 3);
+
+//assignment operators
+let x = 10 + 5;
+console.log(x);
+x += 10;
+console.log(x);
+x *= 4;
+console.log(x);
+x ++;
+x--;
+x--;
+console.log(x);
+
+//Comparision operators shows the boolean value
+console.log(agePratibha > ageDaksh);
+
+console.log(agePratibha < ageDaksh);
+
+console.log(ageDaksh >= 25);
+
