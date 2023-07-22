48b0c6e449d2c61f104c05f13e7e2c57d5f14f69
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2023-07-22 08:33:08 +0700

    math object in JS

---
+++ Ninja Level/mathObject.js
@@ -0,0 +1,12 @@
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.round(7.2));
+console.log(Math.floor(7.9));
+console.log(Math.ceil(7.2));
+console.log(Math.trunc(7.7));
+
+//Math random method
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100));
