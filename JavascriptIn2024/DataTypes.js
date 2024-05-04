4d39aac7116ff5cb735a933c98869b400c15ab32
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2024-05-04 14:30:46 +0700

    revision of Javascript - basic topics

---
+++ JavascriptIn2024/DataTypes.js
@@ -0,0 +1,34 @@
+/*let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); //legacy error on typeof to check the datatype of null
+
+*/
+
+//three different ways to declare variable in JS
+/*
+1. let
+2. const
+3. var
+
+*/
+
+let age = 30;
+age = 31; // re assigning the values Immutable naturally if you use let 
+
+const birthYear = 1991;
+
+//best practice to use const if its never changing and if varible change then use let
+// best practice rule 1. always use const
+
---
+++ JavascriptIn2024/script.js
@@ -0,0 +1,26 @@
+let js = "amazing";
+//if (js === "amazing") alert("Javascript is FUN!");
+console.log(40 + 8 + 23 - 10);
+
+//1. Values (peace of of data) and Variables
+console.log('Hello');
+console.log(23);
+
+//values stored in variables so we can use it multiple times
+
+let firstName = 'Pratibha'; //varible declarations and stored a value in it
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//Variables rules: firstletter is small in one word with camel case and second word of firstletter is in Uppcercase.
+
+let person = 'John';
+let PI = 3.14;
+let myFirstJob = 'programmer';
+let myCurrentJob = 'senior programmer';
+
+//above code shows how to naming the variables in javascript.
+
+console.log(myFirstJob);
+
