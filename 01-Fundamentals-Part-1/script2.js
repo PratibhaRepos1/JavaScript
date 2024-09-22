712ebce6913861703b14f3c353e8e2ae6d1837d2
PratibhaRepos1 <74100289+PratibhaRepos1@users.noreply.github.com>
2024-09-22 14:55:55 +0700

    day 1: ontroduction to JS and data types in js

---
+++ 01-Fundamentals-Part-1/script2.js
@@ -0,0 +1,34 @@
+//variables in javascript
+let name = 'John';
+const age = 30;
+var city = 'Berlin';
+
+//Data Types in javascript
+/*
+1. Primitive types: String, Number, boolean, Null, Undefined, Symbol
+2. Object types: Object, Array, Function, 
+*/
+
+let geeting = 'Hello World!'; //string
+let count = 30; // Number
+let price = 12.45; //Number
+let isActive = true; //Boolean
+let result = null; //Null
+let x; // undefined
+let sym = Symbol('unique'); //Symbol
+
+//object
+let person = {
+    name: 'John',
+    age: 23
+};
+
+//array
+let colors = ['red', 'green', 'blue'];
+
+//Function : the resuable block of code
+function greet() {
+    console.log('Hello');
+}
+
+console.log(sym);
