b20ab21ff3088841eeaedd2038b06663d450e667
Pratibha Jadhav <74100289+PratibhaRepos1@users.noreply.github.com>
2025-02-16 10:47:15 +0700

    day 1 practice

---
+++ JavascriptInfo/day1/variables.js
@@ -0,0 +1,15 @@
+let message;
+
+message = 'Hello World!';
+
+console.log(message); // Hello World!
+
+//multiple variables in one line
+let user = 'John', age = 25, messageME = 'Hello';
+const myBirthday = '18.04.1982';
+
+//uppercase constants
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
